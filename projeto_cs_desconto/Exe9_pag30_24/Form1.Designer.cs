08453b65dc5cae27b9b805300637ccb8983702c1
JoaoPedroPizziolo <84928663+JoaoPedroPizziolo@users.noreply.github.com>
2021-06-07 18:08:01 -0300

    Add files via upload

---
+++ projeto_cs_desconto/Exe9_pag30_24/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Exe9_pag30_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtnprodutos = new System.Windows.Forms.TextBox();
+            this.btncalcule = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantidade de produtos:";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(4, 30);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 2;
+            // 
+            // txtnprodutos
+            // 
+            this.txtnprodutos.Location = new System.Drawing.Point(4, 75);
+            this.txtnprodutos.Name = "txtnprodutos";
+            this.txtnprodutos.Size = new System.Drawing.Size(100, 20);
+            this.txtnprodutos.TabIndex = 3;
+            // 
+            // btncalcule
+            // 
+            this.btncalcule.Location = new System.Drawing.Point(4, 103);
+            this.btncalcule.Name = "btncalcule";
+            this.btncalcule.Size = new System.Drawing.Size(100, 23);
+            this.btncalcule.TabIndex = 4;
+            this.btncalcule.Text = "Calcule";
+            this.btncalcule.UseVisualStyleBackColor = true;
+            this.btncalcule.Click += new System.EventHandler(this.btncalcule_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 138);
+            this.Controls.Add(this.btncalcule);
+            this.Controls.Add(this.txtnprodutos);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtnprodutos;
+        private System.Windows.Forms.Button btncalcule;
+    }
+}
+
---
+++ projeto_cs_desconto/Exe9_pag30_24/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exe9_pag30_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcule_Click(object sender, EventArgs e)
+        {
+            double numero_produtos, calculo, calculo2, desconto, nome;
+
+            nome = double.Parse(txtnome.Text);
+            numero_produtos = double.Parse(txtnprodutos.Text);
+            calculo = numero_produtos * 1.99;
+
+            if (numero_produtos <= 10)
+            {
+                MessageBox.Show(nome.ToString() + Environment.NewLine + "voce gastou: " + calculo.ToString("C") + Environment.NewLine + "Seu desconto sera de 0%");
+            }
+            else if (numero_produtos <= 25 && numero_produtos > 10)
+            {
+                calculo2 = calculo * 0.05;
+                desconto = calculo - calculo2;
+
+                MessageBox.Show(nome.ToString() + Environment.NewLine + "Voce gastou: " + calculo.ToString("C") + Environment.NewLine + "seu desconto sera de 5% e o valor passara ser de: " + desconto.ToString("C"));
+            }
+            else
+            {
+                calculo2 = calculo * 0.12;
+                desconto = calculo - calculo2;
+
+                MessageBox.Show(nome.ToString() + Environment.NewLine + "Voce gastou: " + calculo.ToString("C") + Environment.NewLine + "seu desconto sera de 12% e o valor passara ser de: " + desconto.ToString("C"));
+            }
+        }
+    }
+}
